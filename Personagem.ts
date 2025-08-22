ab70f2648b2c486e291c996bc49c847358d88c20
Arthur Holz <04592380002@senacrs.edu.br>
2025-08-21 21:54:31 -0300

    Adiciona Construtor e método de treino de ataque

--- Personagem.ts
+++ Personagem.ts
@@ -9,4 +9,21 @@ export class Personagem{
     vidaAtual:number = 0;
     vidaMaxima:number = 0;
     poderAtaque:number = 0;
+
+
+constructor(nome: string){
+    this.nome = nome;
+    this.classe = "";
+    this.raca = "";
+    this.nivel = 0;
+    this.arma = "";
+    this.manaAtual = 0;
+    this.manaMaxima = 0;
+    this.vidaAtual = 0;
+    this.vidaMaxima = 0;
+    this.poderAtaque = 0;
+    }
+treinarPoderAtaque(): void {
+    this.poderAtaque += 3 + this.poderAtaque *1.1;
+}
 }
